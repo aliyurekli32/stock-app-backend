2a37fdd509286883190a543b2de1be0f789a6d4c
aliyurekli32 <adamjesperblack@gmail.com>
2023-01-31 17:23:01 +0300

    add: new files

--- main/settings.py
+++ main/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #myapps
+    'rest_framework',
+    
 ]
 
 MIDDLEWARE = [
