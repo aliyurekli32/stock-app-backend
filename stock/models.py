95c3178d1238fc6f081fc96c2f22bb5cd98f09a2
aliyurekli32 <adamjesperblack@gmail.com>
2023-01-31 17:27:19 +0300

    add: brand to models

--- stock/models.py
+++ stock/models.py
@@ -2,9 +2,16 @@ from django.db import models
 
 # Create your models here.
 
-
+#? Table-1 Category
 class Category(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=15)
+    
+    def __str__(self):
+        return f"{self.name}"
+
+#? Table-2 Brand
+class Brand(models.Model):
+    name = models.CharField(max_length=15)
     
     def __str__(self):
         return f"{self.name}"
